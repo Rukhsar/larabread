965f3964f7975d89c1a0ed1c089a5e0128b1f645
Rukhsar <rukhsar.man@gmail.com>
2017-05-16 09:48:45 +0100

    adding values to breadcrumbs

--- src/Contracts/LaraBreadContract.php
+++ src/Contracts/LaraBreadContract.php
@@ -20,4 +20,17 @@ interface LaraBreadContract
      * @return mixed
      */
     public function get($name);
+
+    /**
+     * @param array $items
+     * @return mixed
+     */
+    public function addBread(array $items);
+
+    /**
+     * @param $key
+     * @param $value
+     * @return mixed
+     */
+    public function getDataFromMixed($key, $value);
 }
--- src/LaraBread.php
+++ src/LaraBread.php
@@ -54,4 +54,51 @@ class LaraBread implements LaraBreadContract
     {
         return $this->breads->get($name);
     }
+
+    /**
+     * @param array $items
+     * @return $this
+     */
+    public function addBread(array $items)
+    {
+        foreach ($items as $key => $value)
+        {
+            if ($value instanceof LaraBreadItem) {
+
+                $this->breads->put($value->title, $value);
+
+                continue;
+            }
+
+            list($name, $url) = $this->getDataFromMixed($key, $value);
+
+            $this->add($name, $url);
+        }
+
+        return $this;
+    }
+
+    /**
+     * @param $key
+     * @param $value
+     * @return array
+     */
+    public function getDataFromMixed($key, $value)
+    {
+        if (!is_array($value)) {
+            throw new \InvalidArgumentException("{$key} item is not array");
+        }
+
+        if (count($value) < 2) {
+            throw new \InvalidArgumentException("{$key} item invalid");
+        }
+
+        if (isset($value['name']) && isset($value['url'])) {
+            return [$value['name'], $value['url']];
+        }
+
+        return array_values($value);
+    }
+
+
 }
