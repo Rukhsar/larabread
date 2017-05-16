5eb30b1ca69edbb49d0a7b0a41326ea136eaaaf1
Rukhsar <rukhsar.man@gmail.com>
2017-05-16 10:41:22 +0100

    larabread factory functions added

--- src/Contracts/LaraBreadFactoryContract.php
+++ src/Contracts/LaraBreadFactoryContract.php
@@ -2,7 +2,49 @@
 
 namespace Rukhsar\LaraBread\Contracts;
 
+/**
+ * Interface LaraBreadFactoryContract
+ * @package Rukhsar\LaraBread\Contracts
+ */
 interface LaraBreadFactoryContract
 {
-    public function sayFactory();
+    /**
+     * @param $name
+     * @param $callback
+     * @return mixed
+     */
+    public function add($name, $callback);
+
+    /**
+     * @param $name
+     * @return mixed
+     */
+    public function remove($name);
+
+    /**
+     * @param $name
+     * @return mixed
+     */
+    public function get($name);
+
+    /**
+     * @param $name
+     * @return mixed
+     */
+    public function exists($name);
+
+    /**
+     * @param $name
+     * @param null $template
+     * @return mixed
+     */
+    public function render($name, $template = null);
+
+    /**
+     * @param $name
+     * @param null $template
+     * @return mixed
+     */
+    public function renderIfExists($name, $template = null);
+
 }
--- src/LaraBreadFactory.php
+++ src/LaraBreadFactory.php
@@ -1,12 +1,99 @@
 <?php
 
 namespace Rukhsar\LaraBread;
+
+use Exception;
 use Rukhsar\LaraBread\Contracts\LaraBreadFactoryContract;
 
+/**
+ * Class LaraBreadFactory
+ * @package Rukhsar\LaraBread
+ * @author Rukhsar Manzoor <rukhsar.man@gmail.com>
+ */
 class LaraBreadFactory implements LaraBreadFactoryContract
 {
-    public function sayFactory()
+    /**
+     * @var array
+     */
+    protected $items = [];
+
+    /**
+     * @param $name
+     * @param $callback
+     * @return $this
+     */
+    public function add($name, $callback)
     {
-        echo "I'm a Factory Object";
+        if (!is_callable($callback)) {
+            throw new \InvalidArgumentException();
+        }
+
+        $this->items[$name] = call_user_func_array($callback, [new LaraBread()]);
+
+        return $this;
+    }
+
+    /**
+     * @param $name
+     * @return $this
+     */
+    public function remove($name)
+    {
+        if ($this->exists($name)) {
+            unset($this->items[$name]);
+        }
+
+        return $this;
+    }
+
+    /**
+     * @param $name
+     * @return mixed|null
+     */
+    public function get($name)
+    {
+        if ($this->exists($name)) {
+            return $this->items[$name];
+        }
+
+        return null;
+    }
+
+    /**
+     * @param $name
+     * @return bool
+     */
+    public function exists($name)
+    {
+        return isset($this->items[$name]);
+    }
+
+    /**
+     * @param $name
+     * @param null $template
+     * @return mixed
+     * @throws Exception
+     */
+    public function render($name, $template = null)
+    {
+        if($this->exists($name)) {
+            return $this->items[$name]->render($template);
+        }
+
+        throw new Exception("Breadcrumbs with name = {$name} not exists");
+    }
+
+    /**
+     * @param $name
+     * @param null $template
+     * @return string
+     */
+    public function renderIfExists($name, $template = null)
+    {
+        if ($this->exists($name)) {
+            return $this->items[$name]->render($template);
+        }
+
+        return '';
     }
 }
